46ed3ef5492299638cff6b552190fb97686c1b9c
Fiqri Syah Redha <wibulo27@gmail.com>
2021-04-15 16:04:02 +0700

    Add basic characters crawler

--- disney_fandom_crawler/items.py
+++ disney_fandom_crawler/items.py
@@ -3,10 +3,9 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy import Item, Field
 
 
-class DisneyFandomCrawlerItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class Character(Item):
+    name = Field()
+    thumbnail = Field()
--- disney_fandom_crawler/spiders/characters.py
+++ disney_fandom_crawler/spiders/characters.py
@@ -1,22 +1,31 @@
 import scrapy
-from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import CrawlSpider, Rule
+from disney_fandom_crawler.items import Character
 
 
-class CharactersSpider(CrawlSpider):
-    name = 'characters'
-    allowed_domains = ['https://disney.fandom.com']
-    start_urls = [
-        'http://https://disney.fandom.com/wiki/Category:Disney_characters/']
+class CharactersSpider(scrapy.Spider):
+    name = "characters"
+    allowed_domains = ["disney.fandom.com"]
+    start_urls = ["https://disney.fandom.com/wiki/Category:Disney_characters"]
 
-    rules = (
-        Rule(LinkExtractor(allow=r'Items/'),
-             callback='parse_item', follow=True),
-    )
+    def parse(self, response):
+        skip_this = [
+            "Disney Characters",
+            "Category:",
+            "User blog:",
+            "Disney Prince",
+            "Disney Princess",
+        ]
 
-    def parse_item(self, response):
-        item = {}
-        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
-        #item['name'] = response.xpath('//div[@id="name"]').get()
-        #item['description'] = response.xpath('//div[@id="description"]').get()
-        return item
+        for item in response.css("li.category-page__member"):
+            name = item.css("a.category-page__member-link::text").get()
+
+            if any(el in name for el in skip_this):
+                continue
+
+            thumbnail = item.css("img.category-page__member-thumbnail::attr(src)").get()
+
+            char = Character()
+            char["name"] = name
+            char["thumbnail"] = thumbnail
+
+            yield char
