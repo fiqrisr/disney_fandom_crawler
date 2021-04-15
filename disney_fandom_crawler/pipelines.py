eca18969c527da1657011cb32e3cde20bc25d106
Fiqri Syah Redha <wibulo27@gmail.com>
2021-04-15 17:04:41 +0700

    Add json exporter pipeline

--- disney_fandom_crawler/pipelines.py
+++ disney_fandom_crawler/pipelines.py
@@ -3,11 +3,22 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
+import json
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 
 
-class DisneyFandomCrawlerPipeline:
+class JsonWriterPipeline:
+    def open_spider(self, spider):
+        self.file = open("%s.json" % spider.name, "w")
+        self.file.write("[\n")
+
+    def close_spider(self, spider):
+        self.file.write("]\n")
+        self.file.close()
+
     def process_item(self, item, spider):
+        line = json.dumps(dict(item)) + ",\n"
+        self.file.write(line)
         return item
