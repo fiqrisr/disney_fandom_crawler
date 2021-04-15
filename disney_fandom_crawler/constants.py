03a9bd67a046402f5df2271749a4c3252e14135f
Fiqri Syah Redha <wibulo27@gmail.com>
2021-04-15 22:25:55 +0700

    Refactor some codes

---
+++ disney_fandom_crawler/constants.py
@@ -0,0 +1,14 @@
+selectors = {
+    "CATEGORY_LIST_SELECTOR": "li.category-page__member",
+    "CATEGORY_NAME_SELECTOR": "a.category-page__member-link::text",
+    "CATEGORY_THUMBNAIL_SELECTOR": "img.category-page__member-thumbnail::attr(src)",
+    "CATEGORY_NEXT_PAGE_SELECTOR": "a.category-page__pagination-next::attr(href)",
+}
+
+characters_string_skip_list = [
+    "Disney characters",
+    "Category:",
+    "User blog:",
+    "Disney Prince",
+    "Disney Princess",
+]
--- disney_fandom_crawler/spiders/characters.py
+++ disney_fandom_crawler/spiders/characters.py
@@ -1,5 +1,6 @@
 import scrapy
 from disney_fandom_crawler.items import Character
+from disney_fandom_crawler.constants import selectors, characters_string_skip_list
 
 
 class CharactersSpider(scrapy.Spider):
@@ -8,26 +9,13 @@ class CharactersSpider(scrapy.Spider):
     start_urls = ["https://disney.fandom.com/wiki/Category:Disney_characters"]
 
     def parse(self, response):
-        LIST_SELECTOR = "li.category-page__member"
-        NAME_SELECTOR = "a.category-page__member-link::text"
-        THUMBNAIL_SELECTOR = "img.category-page__member-thumbnail::attr(src)"
-        NEXT_PAGE_SELECTOR = "a.category-page__pagination-next::attr(href)"
+        for item in response.css(selectors["CATEGORY_LIST_SELECTOR"]):
+            name = item.css(selectors["CATEGORY_NAME_SELECTOR"]).get()
 
-        skip_this = [
-            "Disney characters",
-            "Category:",
-            "User blog:",
-            "Disney Prince",
-            "Disney Princess",
-        ]
-
-        for item in response.css(LIST_SELECTOR):
-            name = item.css(NAME_SELECTOR).get()
-
-            if any(el in name for el in skip_this):
+            if any(el in name for el in characters_string_skip_list):
                 continue
 
-            thumbnail = item.css(THUMBNAIL_SELECTOR).get()
+            thumbnail = item.css(selectors["CATEGORY_THUMBNAIL_SELECTOR"]).get()
 
             char = Character()
             char["name"] = name
@@ -35,7 +23,7 @@ class CharactersSpider(scrapy.Spider):
 
             yield char
 
-        next_page = response.css(NEXT_PAGE_SELECTOR)
+        next_page = response.css(selectors["CATEGORY_NEXT_PAGE_SELECTOR"])
 
         if next_page:
             yield from response.follow_all(next_page, self.parse)
