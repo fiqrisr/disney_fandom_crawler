53c52814207a7988864f1e8f12eb46440a9c6974
Fiqri Syah Redha <wibulo27@gmail.com>
2021-04-16 15:01:14 +0700

    Use item loader

--- disney_fandom_crawler/spiders/characters.py
+++ disney_fandom_crawler/spiders/characters.py
@@ -20,23 +20,32 @@ class CharactersSpider(scrapy.Spider):
             url = item.css(selectors["CATEGORY_URL"])
 
             char = Character()
-            char["name"] = name
             char["thumbnail"] = thumbnail
 
             yield from response.follow_all(
                 url, self.parse_detail, cb_kwargs=dict(char=char)
             )
 
-        next_page = response.css(selectors["CATEGORY_NEXT_PAGE"])
+        # next_page = response.css(selectors["CATEGORY_NEXT_PAGE"])
 
-        if next_page:
-            yield from response.follow_all(next_page, self.parse)
+        # if next_page:
+        #     yield from response.follow_all(next_page, self.parse)
 
     def parse_detail(self, response, char):
-        char["url"] = response.url
-        char["feature_films"] = response.css(
-            selectors["CHARACTERS_FEATURE_FILMS"]
-        ).getall()
-        char["short_films"] = response.css(selectors["CHARACTERS_SHORT_FILMS"]).getall()
+        loader = ItemLoader(item=char, response=response)
 
+        loader.add_value("url", response.url)
+        loader.add_css("image", selectors["CHARACTER_IMAGE"])
+        loader.add_css("name", selectors["CHARACTER_NAME"])
+        loader.add_css("feature_films", selectors["CHARACTER_FEATURE_FILMS"])
+        loader.add_css("short_films", selectors["CHARACTER_SHORT_FILMS"])
+        loader.add_css("shows", selectors["CHARACTER_SHOWS"])
+
+        if len(loader.get_css(selectors["CHARACTER_NAME"])) < 1:
+            loader.add_css("name", selectors["PAGE_HEADER_TITLE"])
+
+        if len(loader.get_css(selectors["CHARACTER_IMAGE"])) < 1:
+            loader.add_css("image", selectors["CHARACTER_THUMB_IMAGE"])
+
+        char = loader.load_item()
         yield char
